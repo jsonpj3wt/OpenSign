56c42a18709d756b73abcd774293e679ad1aa435
Ryan Tilly <rtilly@firstlighthomecare.com>
2024-07-19 16:13:59 -0500

    Use SERVER_URL environment variable for migrations.

--- apps/OpenSignServer/runmigrate.js
+++ apps/OpenSignServer/runmigrate.js
@@ -1,9 +1,7 @@
 // const { exec } = require('child_process');
 import { exec } from 'child_process';
 
-const migrate =
-  'APPLICATION_ID=legadranaxn SERVER_URL=http://localhost:8080/app MASTER_KEY=XnAwPDRQQyMr npx parse-dbtool migrate';
-// `APPLICATION_ID=${process.env.APP_ID} SERVER_URL=${process.env.SERVER_URL} MASTER_KEY=${process.env.MASTER_KEY} npx parse-dbtool migrate`;
+const migrate = `APPLICATION_ID=${process.env.APP_ID} SERVER_URL=${process.env.SERVER_URL} MASTER_KEY=${process.env.MASTER_KEY} npx parse-dbtool migrate`;
 
 exec(migrate, (error, stdout, stderr) => {
   if (error) {
@@ -20,7 +18,7 @@ exec(migrate, (error, stdout, stderr) => {
 });
 
 // const seeder =
-//   'APPLICATION_ID=legadranaxn SERVER_URL=http://localhost:8080/app MASTER_KEY=XnAwPDRQQyMr npx parse-dbtool seed';
+//   `APPLICATION_ID=${process.env.APP_ID} SERVER_URL=${process.env.SERVER_URL} MASTER_KEY=${process.env.MASTER_KEY} npx parse-dbtool migrate`;
 
 // exec(seeder, (error, stdout, stderr) => {
 //   if (error) {
