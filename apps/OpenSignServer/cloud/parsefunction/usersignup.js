a01bd2a3dab31b594709e74936cfcc0be5b9d063
Jason Pugh <jpugh@firstlighthomecare.com>
2024-08-02 15:54:00 -0500

    revert that change until I figure out what is going on here...

--- apps/OpenSignServer/cloud/parsefunction/usersignup.js
+++ apps/OpenSignServer/cloud/parsefunction/usersignup.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { cloudServerUrl } from '../../Utils.js';
-const serverUrl = process.env.SERVER_URL; //cloudServerUrl;
+const serverUrl = cloudServerUrl; //process.env.SERVER_URL;
 const APPID = process.env.APP_ID;
 const masterKEY = process.env.MASTER_KEY;
 
