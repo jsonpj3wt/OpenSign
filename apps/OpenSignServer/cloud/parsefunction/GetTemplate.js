0c27107f73542f3547eefa141b0afd278cf2c19e
Jason Pugh <jpugh@firstlighthomecare.com>
2024-08-02 17:30:28 -0500

    revert back

--- apps/OpenSignServer/cloud/parsefunction/GetTemplate.js
+++ apps/OpenSignServer/cloud/parsefunction/GetTemplate.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { cloudServerUrl } from '../../Utils.js';
 
 export default async function GetTemplate(request) {
-  const serverUrl = process.env.SERVER_URL; //cloudServerUrl;
+  const serverUrl = cloudServerUrl; //process.env.SERVER_URL;
   const templateId = request.params.templateId;
   const ispublic = request.params.ispublic;
 
